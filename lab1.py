89178c6d5a16306b0b02812085ef86e975117d6b
junxpected <megasharik90@gmail.com>
2025-09-08 22:43:08 +0300

    Lab1: додано клас Phone у папку lab1v7

--- lab1.py
+++ lab1.py
@@ -1,22 +1,29 @@
-class Figure:
-    def __init__(self, name, area):
-        self.__name = name      
-        self._area = area       
-        print(f"Фігура {name} створена.")
+class Phone:
+    def __init__(self, brand, model, battery_level=100):
+        self.__brand = brand       
+        self.__model = model       
+        self._battery_level = battery_level  
+        print(f"Телефон {self.__brand} {self.__model} створений.")
 
     @property
-    def area(self):
-        return self._area
+    def battery_level(self):
+        return self._battery_level
 
-    def get_figure(self):
-        return f"Назва: {self.__name}, Площа: {self._area}"
+    def call(self, number):
+        if self._battery_level > 0:
+            self._battery_level -= 10  
+            return f"📞 Дзвінок на {number}... Залишок батареї: {self._battery_level}%"
+        else:
+            return "❌ Батарея розряджена!"
 
     def __del__(self):
-        print(f"Фігура {self.__name} видалена.")
+        print(f"Телефон {self.__brand} {self.__model} вимкнено.")
 
 
-f1 = Figure("Квадрат", 25)
-f2 = Figure("Коло", 78.5)
+p1 = Phone("Apple", "iPhone 13")
+p2 = Phone("Samsung", "Galaxy S22", battery_level=50)
 
-print(f1.get_figure())
-print(f2.get_figure())
+print(p1.call("+380971112233"))
+print(p1.call("+380501234567"))
+
+print(p2.call("+380631234567"))
